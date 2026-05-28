5999485a587bc2fb80f473175968ff9c0bcd9a5a
ROYALKINGKUNAL9090 <131905488+ROYALKINGKUNAL9090@users.noreply.github.com>
2026-05-28 23:35:08 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 st.title("My AI App")
 
-GOOGLE_API_KEY = "AIzaSyDfKPmbXqfLQtP6eWcdAqbUPUhu4MJjOBI"
+GOOGLE_API_KEY = "AIzaSyClqufiEjImdlm_iU6avdStm7GdXu0_yL4"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 user_input = st.text_input("Ask me anything:")
