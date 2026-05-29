7391d6156e0daac27fa4b3ebcd8d4b7ef89da999
ROYALKINGKUNAL9090 <131905488+ROYALKINGKUNAL9090@users.noreply.github.com>
2026-05-29 19:20:24 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 
-st.title("My AI App")
+st.title("ODISHA AI App")
 
 GOOGLE_API_KEY = "AIzaSyClqufiEjImdlm_iU6avdStm7GdXu0_yL4"
 genai.configure(api_key=GOOGLE_API_KEY)
